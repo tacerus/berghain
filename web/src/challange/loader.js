3c2a898ad19e9082625b0eb127d828a462106169
Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>
2025-05-28 13:37:35 +0200

    Customization for openSUSE
    Branding and reduced countdown.
    
    Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

--- web/src/challange/loader.js
+++ web/src/challange/loader.js
@@ -6,6 +6,8 @@
  * Start the countdown.
  */
 export function start(){
+    const container =  /** @type {HTMLDivElement} */ (document.querySelector(".captcha-container"));
+    container.classList.add("alert-warning");
     const loader = /** @type {HTMLDivElement} */ (document.querySelector(".circle-loader"));
     loader.style.visibility = "visible";
 }
@@ -28,19 +30,26 @@ export function setChallengeInfo(text){
 export function stop(failed = false){
     const loader = /** @type {HTMLDivElement} */ (document.querySelector(".circle-loader"));
     const checkmark = /** @type {HTMLDivElement} */ (document.querySelector(".checkmark"));
+    const container =  /** @type {HTMLDivElement} */ (document.querySelector(".captcha-container"));
     const cross = /** @type {HTMLDivElement} */ (document.querySelector(".cross"));
 
     loader.classList.add("load-complete");
+    container.classList.remove("alert-warning");
 
     failed
         ? cross.style.display = "block"
         : checkmark.style.display = "block";
 
     if (failed){
+        container.classList.add("alert-danger");
+        setChallengeInfo("Challenge failed.");
         return;
     }
 
-    let countdown = 4;
+    setChallengeInfo("Challenge succeeded.");
+    container.classList.add("alert-success");
+
+    let countdown = 3;
     const interval = setInterval(() => {
         countdown--;
 
